aa65c0798cb57cf0083ca6cae5897cc3c5e573a7
S10222254@connect.np.edu.sg <S10222254@connect.np.edu.sg>
2022-01-18 16:20:00 +0800

    created js function to select hours, min, sec

--- javascript.js
+++ javascript.js
@@ -1,3 +1,12 @@
 (() => {
+const hours = Selector(".hours");
+const minutes = Selector(".minutes");
+const seconds = Selector(".seconds");
 
 })();
+
+//instead writing out one by one for each hours, minutes and seconds, i can create this function to select all
+function Selector(element)
+{
+    return document.querySelector(element);
+}
