a100c5dacb4d3b6254ba709c8af670f9fe612bef
sackslasher <s10227699@connect.np.edu.sg>
2022-02-07 12:59:23 +0800

    removed the read More function

--- mainIndex.js
+++ mainIndex.js
@@ -42,19 +42,5 @@ function openUrlInNewTab(htmlpage) {
     window.open(htmlpage, "_blank");
 }
 
-//Function to expand hidden text
-$(document).ready(function(){
-  $(".read").click(function(){
-      $(this).prev().toggle();
-      $(this).siblings('.dots').
-          toggle();
-      if($(this).text()== 'Read more'){
-          $(this).text('read less');
-      }
-      else
-      {
-          $(this).text('read more');
-      }
-  });
-});
+
 
